c3889ed949e8f24b2b4ae625793be3a2208386ff
James-Daykin <james.daykin@dedalus.com>
2024-05-17 17:55:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+// server.js
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+// Sample species data
+const speciesData = [
+  {
+    name: "Amur Leopard",
+    conservationStatus: "Critically Endangered",
+    continent: "Asia",
+    populationTrend: -0.03,
+    image:
+      "https://wildscapia.com/wp-content/uploads/2020/05/amur-leopard-4112011_1280.jpg",
+  },
+  {
+    name: "Black Rhino",
+    conservationStatus: "Critically Endangered",
+    continent: "Africa",
+    populationTrend: -0.05,
+    image: "",
+  },
+  {
+    name: "Vaquita",
+    conservationStatus: "Critically Endangered",
+    continent: "North America",
+    populationTrend: -0.05,
+    image: "",
+  },
+  {
+    name: "Sumatran Elephant",
+    conservationStatus: "Critically Endangered",
+    continent: "Asia",
+    populationTrend: -0.04,
+    image: "",
+  },
+  {
+    name: "Javan Rhino",
+    conservationStatus: "Critically Endangered",
+    continent: "Asia",
+    populationTrend: -0.06,
+    image: "",
+  },
+  {
+    name: "Mountain Gorilla",
+    conservationStatus: "Critically Endangered",
+    continent: "Africa",
+    populationTrend: 0.02,
+    image: "",
+  },
+  {
+    name: "Hawksbill Turtle",
+    conservationStatus: "Critically Endangered",
+    continent: "South America",
+    populationTrend: -0.07,
+    image: "",
+  },
+  {
+    name: "Philippine Eagle",
+    conservationStatus: "Critically Endangered",
+    continent: "Asia",
+    populationTrend: -0.04,
+    image: "",
+  },
+  {
+    name: "Yangtze Finless Porpoise",
+    conservationStatus: "Critically Endangered",
+    continent: "Asia",
+    populationTrend: -0.03,
+    image: "",
+  },
+  {
+    name: "Cross River Gorilla",
+    conservationStatus: "Critically Endangered",
+    continent: "Africa",
+    populationTrend: -0.03,
+    image: "",
+  },
+];
+
+// Route to get top 5 endangered species for a given continent
+app.get("/endangered/:continent", (req, res) => {
+  const { continent } = req.params;
+  const continentSpecies = speciesData.filter(
+    (species) => species.continent === continent
+  );
+  const sortedSpecies = continentSpecies
+    .sort((a, b) => b.populationTrend - a.populationTrend)
+    .slice(0, 5);
+  res.json(sortedSpecies);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
