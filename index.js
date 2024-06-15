31ecdf3e77569ecb6bcb4179a0233df4a99ce993
James-Daykin <james.daykin@dedalus.com>
2024-06-15 10:22:28 +0100

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3001;
+const PORT = "https://intlayer-ltwv15yft-james-daykins-projects.vercel.app/";
 
 // CORS configuration
 const corsOptions = {
