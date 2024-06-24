487019b69260c72276da116244d33015f9c5ae9f
James-Daykin <james.daykin@dedalus.com>
2024-06-24 05:58:37 +0100

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = "https://intlayer-m8ehrb3ty-james-daykins-projects.vercel.app";
+const PORT = "https://intlayer-ru6a2okon-james-daykins-projects.vercel.app/";
 
 // CORS configuration
 const corsOptions = {
@@ -15,24 +15,24 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Sample species data
-const speciesData = [
+const moviesData = [
   {
     id: 1,
     name: "Mean Girls",
-    conservationStatus: "https://www.youtube.com/watch?v=oDU84nmSDZY",
-    continent: "Comedy",
-    populationTrend: "4/5",
+    category: "Comedy",
+    rating: "4/5",
+    trailer: "https://www.youtube.com/watch?v=oDU84nmSDZY",
     image:
       "https://media.themoviedb.org/t/p/w600_and_h900_bestv2/fXm3YKXAEjx7d2tIWDg9TfRZtsU.jpg",
     description:
-      "Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.",
+      "Cady Heron. is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.",
   },
   {
     id: 2,
     name: "South Park",
-    conservationStatus: "https://www.youtube.com/watch?v=PbMl6DjhJ1I",
-    continent: "Comedy",
-    populationTrend: "4/5",
+    category: "Comedy",
+    rating: "4/5",
+    trailer: "https://www.youtube.com/watch?v=PbMl6DjhJ1I",
     image: "https://flxt.tmsimg.com/assets/p11860901_p_v8_ac.jpg",
     description:
       "South Park: Bigger, Longer & Uncut is a 1999 American adult animated musical comedy film based on the animated sitcom South Park.",
@@ -40,33 +40,31 @@ const speciesData = [
   {
     id: 3,
     name: "Austin Powers",
-    conservationStatus: "https://www.youtube.com/watch?v=LGVjoLlgHbM",
-    continent: "Comedy",
-    populationTrend: "4.5/5",
+    category: "Comedy",
+    rating: "4.5/5",
+    trailer: "https://www.youtube.com/watch?v=LGVjoLlgHbM",
     image:
       "https://th.bing.com/th/id/OIP.phQs102CS-Qp44IqlRaWrAHaLL?w=110&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     description:
       "Austin Powers: The Spy Who Shagged Me is a 1999 American spy comedy film directed by Jay Roach. It is the second installment in the Austin Powers film series, after International Man of Mystery.",
   },
-
   {
     id: 4,
     name: "Dumb and Dumber",
-    conservationStatus: "https://www.youtube.com/watch?v=l13yPhimE3o",
-    continent: "Comedy",
-    populationTrend: "4.3/5",
+    category: "Comedy",
+    rating: "4.3/5",
+    trailer: "https://www.youtube.com/watch?v=l13yPhimE3o",
     image:
       "https://th.bing.com/th/id/OIP.jUcrqHwAZWBEX-bDbIytogHaLH?w=115&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     description:
       "Dumb and Dumber is a 1994 American buddy comedy film directed by Peter Farrelly. The film stars Jim Carrey and Jeff Daniels as dimwitted friends who set out on a cross-country trip to return a briefcase full of money.",
   },
-
   {
     id: 6,
     name: "There's Something About Mary",
-    conservationStatus: "https://www.youtube.com/watch?v=20PQBtyfNZY",
-    continent: "Comedy",
-    populationTrend: "4.4/5",
+    category: "Comedy",
+    rating: "4.4/5",
+    trailer: "https://www.youtube.com/watch?v=20PQBtyfNZY",
     image:
       "https://th.bing.com/th/id/OIP.IGybD_B24y6fl8fgdFYH5AHaLH?w=115&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     description:
@@ -75,9 +73,9 @@ const speciesData = [
   {
     id: 7,
     name: "Office Space",
-    conservationStatus: "https://www.youtube.com/watch?v=dMIrlP61Z9s",
-    continent: "Comedy",
-    populationTrend: "4.6/5",
+    category: "Comedy",
+    rating: "4.6/5",
+    trailer: "https://www.youtube.com/watch?v=dMIrlP61Z9s",
     image:
       "https://th.bing.com/th/id/OIP.KNW8B7FRtQybBZSLW_0KYwHaK-?w=120&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     description:
@@ -86,9 +84,9 @@ const speciesData = [
   {
     id: 8,
     name: "Anchorman: The Legend of Ron Burgundy",
-    conservationStatus: "https://www.youtube.com/watch?v=NJQ4qEWm9lU",
-    continent: "Comedy",
-    populationTrend: "4.5/5",
+    category: "Comedy",
+    rating: "4.5/5",
+    trailer: "https://www.youtube.com/watch?v=NJQ4qEWm9lU",
     image:
       "https://th.bing.com/th/id/OIP.Qk3kSxnbkvxLFvg4ljORjAHaLH?w=115&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     description:
@@ -97,9 +95,9 @@ const speciesData = [
   {
     id: 9,
     name: "American Pie",
-    conservationStatus: "https://www.youtube.com/watch?v=iUZ3Yxok6N8",
-    continent: "Comedy",
-    populationTrend: "4.2/5",
+    category: "Comedy",
+    rating: "4.2/5",
+    trailer: "https://www.youtube.com/watch?v=iUZ3Yxok6N8",
     image:
       "https://th.bing.com/th/id/OIP.eL8k82OAsPyHdGuQLxemNAHaLH?w=115&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     description:
@@ -107,36 +105,34 @@ const speciesData = [
   },
 ];
 
-// Array with just the name and description of each species
-const speciesNamesAndDescriptions = speciesData.map((species) => ({
-  id: species.id,
-  name: species.name,
-  description: species.description,
-  image: species.image,
+// Array with just the name and description of each movie
+const moviesNamesAndDescriptions = moviesData.map((movie) => ({
+  id: movie.id,
+  name: movie.name,
+  description: movie.description,
+  image: movie.image,
 }));
 
-// Route to get top 5 endangered species for a given continent
-app.get("/endangered/:continent", (req, res) => {
-  const { continent } = req.params;
-  const continentSpecies = speciesData.filter(
-    (species) => species.continent === continent
+// Route to get top movies for a given category
+app.get("/movies/:category", (req, res) => {
+  const { category } = req.params;
+  const categoryMovies = moviesData.filter(
+    (movie) => movie.category === category
   );
-  const sortedSpecies = continentSpecies
-    .sort((a, b) => b.populationTrend - a.populationTrend)
-    .slice(0, 5);
-  res.json(sortedSpecies);
+
+  res.json(categoryMovies);
 });
 
 // Route to get name and description by id
-app.get("/species-descriptions/:id", (req, res) => {
+app.get("/movie-details/:id", (req, res) => {
   const { id } = req.params;
-  const species = speciesNamesAndDescriptions.find(
-    (species) => species.id === parseInt(id)
+  const movie = moviesNamesAndDescriptions.find(
+    (movie) => movie.id === parseInt(id)
   );
-  if (species) {
-    res.json(species);
+  if (movie) {
+    res.json(movie);
   } else {
-    res.status(404).send({ error: "Species not found" });
+    res.status(404).send({ error: "Movie not found" });
   }
 });
 
