9baed18fcac612c01a1bfb1bf9a63220118457b4
James-Daykin <james.daykin@dedalus.eu>
2024-08-06 18:26:44 +0100

    deleted text

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 3000; // Updated PORT to a valid number for local development
+const PORT = 3000;
 
 // CORS configuration
 const corsOptions = {
